ed02004ece716e7a79bdecb357a1b8ecc3f9ca15
Bismilah Langford <bismilah.lf@gmail.com>
2020-09-08 19:02:26 +0200

    Create first component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+        <form action=" " className="store-picker">
+            <h2>Please, Enter a Store</h2>
+        </form>);
+    }
+}
+
+export default StorePicker;
