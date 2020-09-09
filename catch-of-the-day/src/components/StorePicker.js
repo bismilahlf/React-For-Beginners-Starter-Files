1af2c79a92389241b7aa2f99a954def4b05f2f7f
Bismilah Langford <bismilah.lf@gmail.com>
2020-09-09 18:43:52 +0200

    Add components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,9 +3,14 @@ import React from 'react';
 class StorePicker extends React.Component {
     render() {
         return (
-        <form action=" " className="store-picker">
-            <h2>Please, Enter a Store</h2>
-        </form>);
+            <React.Fragment>
+                <form action=" " className="store-picker">
+                    <h2>Please Enter a Store</h2>
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type="submit">Visit Store</button>
+                </form>
+            </React.Fragment>
+        );
     }
 }
 
